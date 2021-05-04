dfabeb39c0b8082ad51cd7ec29094f3e21a0eaf9
gbielanova <gbielanova@gmail.com>
2021-05-04 09:30:37 +0200

    Update variable name

--- tests/test_play.py
+++ tests/test_play.py
@@ -20,19 +20,19 @@ class TestClass:
         }
 
     def test_play_keep(self):
-        mock_request_data = {
+        data = {
             'choose_option': 'keep',
             'attempts': 100000
         }
-        response = self.client.post(self.url, data=json.dumps(mock_request_data), headers=self.headers)
+        response = self.client.post(self.url, data=json.dumps(data), headers=self.headers)
         assert response.status_code == 200
         assert abs(33 - response.json['wins'] / (response.json['wins'] + response.json['loose']) * 100) < 1
 
     def test_play_change(self):
-        mock_request_data = {
+        data = {
             'choose_option': 'change',
             'attempts': 100000
         }
-        response = self.client.post(self.url, data=json.dumps(mock_request_data), headers=self.headers)
+        response = self.client.post(self.url, data=json.dumps(data), headers=self.headers)
         assert response.status_code == 200
         assert abs(66 - response.json['wins'] / (response.json['wins'] + response.json['loose']) * 100) < 1
