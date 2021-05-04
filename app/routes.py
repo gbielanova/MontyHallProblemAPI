62e00304c1024455ab3f5f065df6cf634d72be7f
gbielanova <gbielanova@gmail.com>
2021-05-04 10:19:25 +0200

    Add proper validations

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,34 @@
 from flask import Flask, jsonify, request, abort
 from paradox import simulate_paradox
+from marshmallow import Schema, fields, post_load, ValidationError, validates, validate
+
+OPTIONS = ['keep', 'change']
+
+
+class Game:
+    def __init__(self, choose_option, attempts):
+        self.choose_option = choose_option
+        self.attempts = attempts
+
+
+class GameSchema(Schema):
+    choose_option = fields.String(required=True)
+    attempts = fields.Integer(required=True)
+
+    @validates('choose_option')
+    def validate_choice(self, choose_option):
+        if choose_option not in OPTIONS:
+            raise ValidationError('Unsupported choose_option')
+
+    @validates('attempts')
+    def validate_attempts(self, attempts):
+        if attempts <= 0:
+            raise ValidationError('Incorrect attempts')
+
+    @post_load
+    def create_game(self, data, **kwargs):
+        return Game(data['choose_option'], data['attempts'])
+
 
 def configure_routes(app):
     # POST /play/
@@ -10,13 +39,12 @@ def configure_routes(app):
     def play():
         request_data = request.get_json()
 
-        if request_data['choose_option'] not in ['change', 'keep']:
-            return 'Invalid choose_option, available options are change and keep', 400
         try:
-            if int(request_data['attempts']) <= 0:
-                return 'Invalid attempts, attempts should be positive', 400
-        except ValueError:
-            return 'Invalid attempts, attempts should be int', 400
+            schema = GameSchema()
+            data = schema.load(request_data)
+            game = schema.dump(data)
+            print(type(game))
+        except ValidationError as error:
+            return error.messages, 400
 
-        return jsonify(simulate_paradox(request_data['choose_option'],
-                                        int(request_data['attempts'])))
+        return jsonify(simulate_paradox(game['choose_option'], game['attempts']))
